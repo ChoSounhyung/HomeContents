2410211b4df131c2fe5657d65f4dace5ee459e56
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-04 01:36:23 +0900

    Main nav 동작 설정

--- app/src/main/java/com/sacol/homecontents/MainActivity.java
+++ app/src/main/java/com/sacol/homecontents/MainActivity.java
@@ -1,8 +1,13 @@
 package com.sacol.homecontents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Set home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        return true;
+                    case R.id.hashtag:
+                        startActivity(new Intent(getApplicationContext(), HashtagActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.plus:
+                        startActivity(new Intent(getApplicationContext(), PlusActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.storage:
+                        startActivity(new Intent(getApplicationContext(), StorageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.mypage:
+                        startActivity(new Intent(getApplicationContext(), MypageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
