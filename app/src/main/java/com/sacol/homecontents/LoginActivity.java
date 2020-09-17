e892065302ba006b6d0a28d90abbca5da54446e8
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-17 22:37:39 +0900

    로그인, 가입하기 인텐트 연결

--- app/src/main/java/com/sacol/homecontents/LoginActivity.java
+++ app/src/main/java/com/sacol/homecontents/LoginActivity.java
@@ -2,13 +2,50 @@ package com.sacol.homecontents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginBtn;
+    private TextView signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+        setUp();
     }
+
+    private void init() {
+        loginBtn = findViewById(R.id.login_btn);
+        signupBtn = findViewById(R.id.login_signup);
+    }
+
+    private void setUp() {
+        loginBtn.setOnClickListener(goMainPage);
+        signupBtn.setOnClickListener(goSignupPage);
+    }
+
+    View.OnClickListener goMainPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener goSignupPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        }
+    };
 }
