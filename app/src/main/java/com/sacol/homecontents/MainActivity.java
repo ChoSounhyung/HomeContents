67c0fba106ade8473e3c9b023587fb43d9d54d47
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-17 22:38:55 +0900

    로그인, 회원가입 페이지로 가는 인텐트 연결

--- app/src/main/java/com/sacol/homecontents/MainActivity.java
+++ app/src/main/java/com/sacol/homecontents/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView login;
+    private TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setBottomNav();
+        init();
+        setUp();
+    }
+
+    private void init() {
+        login = findViewById(R.id.go_login);
+        signup = findViewById(R.id.go_signup);
+    }
+
+    private void setUp() {
+        login.setOnClickListener(goLoginPage);
+        signup.setOnClickListener(goSignupPage);
     }
 
     private void setBottomNav() {
@@ -54,4 +70,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    View.OnClickListener goLoginPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener goSignupPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        }
+    };
 }
