cab9e423d1b2adc76bb073e39a1aa52da1ca7210
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-19 00:32:32 +0900

    intent 연결

--- app/src/main/java/com/sacol/homecontents/SignupActivity.java
+++ app/src/main/java/com/sacol/homecontents/SignupActivity.java
@@ -2,13 +2,47 @@ package com.sacol.homecontents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    private TextView loginBtn;
+    private ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        init();
+        setUp();
     }
+
+    private void init() {
+        loginBtn = findViewById(R.id.signup_login);
+        backBtn = findViewById(R.id.signup_back);
+    }
+
+    private void setUp() {
+        loginBtn.setOnClickListener(goLoginPage);
+        backBtn.setOnClickListener(goBackPage);
+    }
+
+    View.OnClickListener goLoginPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            SignupActivity.this.finish();
+        }
+    };
 }
