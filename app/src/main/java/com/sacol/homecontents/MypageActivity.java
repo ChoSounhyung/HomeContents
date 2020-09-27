72fa90f2f002b497a118923495df663be7fbc30b
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-27 17:15:09 +0900

    setBottomNav() 삭제, 필요없는 주석 제거

--- app/src/main/java/com/sacol/homecontents/MypageActivity.java
+++ app/src/main/java/com/sacol/homecontents/MypageActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,7 +12,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -33,11 +31,12 @@ public class MypageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
-        setBottomNav();
+
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         uid = user.getUid();
         db = FirebaseFirestore.getInstance();
+
         init();
         setUp();
         userDate();
@@ -86,41 +85,6 @@ public class MypageActivity extends AppCompatActivity {
         }
     };
 
-    private void setBottomNav() {
-        //Initialize and assign variable
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
-
-        //Set mypage selected
-        bottomNavigationView.setSelectedItemId(R.id.mypage);
-
-        //Perform ItemSelectedListener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.hashtag:
-                        startActivity(new Intent(getApplicationContext(), HashtagActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.plus:
-                        startActivity(new Intent(getApplicationContext(), PlusActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.storage:
-                        startActivity(new Intent(getApplicationContext(), StorageActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.mypage:
-                        return true;
-                }
-                return false;
-            }
-        });
-    }
     private void startSettingActivity(){
         Intent intent = new Intent(this, SettingActivity.class);
         startActivity(intent);
