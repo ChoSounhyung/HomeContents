11ffcaf76b16b1a3e4fe36d4c954ab84320ce498
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-09-29 00:00:49 +0900

    intent 연결, Toast 추가

--- app/src/main/java/com/sacol/homecontents/PlusActivity.java
+++ app/src/main/java/com/sacol/homecontents/PlusActivity.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PlusActivity extends AppCompatActivity {
     private ImageView cancelBtn;
+    private TextView shareBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +23,27 @@ public class PlusActivity extends AppCompatActivity {
         setContentView(R.layout.activity_plus);
 
         cancelBtn = findViewById(R.id.plus_cancel);
+        shareBtn = findViewById(R.id.plus_share);
+
         cancelBtn.setOnClickListener(cancel);
+        shareBtn.setOnClickListener(share);
+
     }
 
     View.OnClickListener cancel = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            finish();
+            Toast.makeText(PlusActivity.this, "컨텐츠 작성을 취소합니다", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener share = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
             Intent intent = new Intent(PlusActivity.this, MainActivity.class);
             startActivity(intent);
+            Toast.makeText(PlusActivity.this, "컨텐츠가 공유되었습니다", Toast.LENGTH_SHORT).show();
         }
     };
 }
