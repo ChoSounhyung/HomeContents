45b2fb14fb42a8f76380da6a21cae34cdbd48113
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-28 23:44:25 +0900

    DetailAdapter 구현, icon 추가

--- app/src/main/java/adapter/DetailAdapter.java
+++ app/src/main/java/adapter/DetailAdapter.java
@@ -1,4 +1,60 @@
 package adapter;
 
-public class DetailAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.sacol.homecontents.DetailActivity;
+import com.sacol.homecontents.Model;
+import com.sacol.homecontents.R;
+
+import java.util.List;
+
+public class DetailAdapter extends PagerAdapter {
+    private List<Model> models;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public DetailAdapter(List<Model> models, Context context) {
+        this.models = models;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull final ViewGroup container, final int position) {
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.detail_image_item, container, false);
+
+        ImageView image;
+
+        image = view.findViewById(R.id.detail_image);
+        image.setImageResource(models.get(position).getImage());
+
+        container.addView(view, 0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
 }
