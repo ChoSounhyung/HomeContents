c8f6624edb3d2ec8a015af44dd71d1d076154b28
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-10-29 04:17:05 +0900

    search, mystorage intent 연결

--- app/src/main/java/com/sacol/homecontents/SearchActivity.java
+++ app/src/main/java/com/sacol/homecontents/SearchActivity.java
@@ -1,13 +1,35 @@
 package com.sacol.homecontents;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+    private TextView cancel_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        init();
+        setUp();
     }
+
+    public void init() {
+        cancel_btn = findViewById(R.id.search_cancel);
+    }
+
+    public void setUp() {
+        cancel_btn.setOnClickListener(goBackPage);
+    }
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            SearchActivity.this.finish();
+        }
+    };
 }
