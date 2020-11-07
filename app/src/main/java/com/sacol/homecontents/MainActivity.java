1f441cdbc06afec0a1c1f33e715e38f4582a1371
jimin-oh <wlals6828@daum.net>
2020-11-08 00:17:07 +0900

    dashboardItem text with firebase

--- app/src/main/java/com/sacol/homecontents/MainActivity.java
+++ app/src/main/java/com/sacol/homecontents/MainActivity.java
@@ -2,24 +2,14 @@ package com.sacol.homecontents;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewParent;
-import android.widget.Adapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -43,33 +33,22 @@ public class MainActivity extends AppCompatActivity {
     private ImageView search_btn;
     private RelativeLayout go_mypage;
     private RelativeLayout go_mystorage;
-    String title;
-    String content;
-    int img;
-    private DatabaseReference databaseRefernece;
 
+    private DatabaseReference databaseRefernece;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        models.add(new Model(R.drawable.sample1, "5000번 저어 만드는 오므라이스", "asdfasdf00"));
-//        models.add(new Model(R.drawable.sample2, "아무노래 챌린지", "sdfgsdfg11"));
-//        models.add(new Model(R.drawable.sample3, "달고나 커피 만들기", "qwerqwer22"));
-//        models.add(new Model(R.drawable.sample4, "피포페인팅", "zxcvzxcv33"));
-//
-//        mainAdapter = new MainAdapter(models, this);
+        //Firebase
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startSignupActivity();
+        }
 
         init();
         setUp();
         initDatabase();
-        models.add(new Model(R.drawable.sample1, title,content));
 
-        //Firebase
-        mainAdapter = new MainAdapter(models, this);
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-            startSignupActivity();
-        }
     }
 
     public void init() {
@@ -78,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
         search_btn = findViewById(R.id.search_btn);
         go_mypage = findViewById(R.id.go_mypage);
         go_mystorage = findViewById(R.id.go_mystrorage);
+        databaseRefernece = FirebaseDatabase.getInstance().getReference();
+        models = new ArrayList<>();
+        mainAdapter = new MainAdapter(models, this);
 
     }
 
@@ -88,8 +70,6 @@ public class MainActivity extends AppCompatActivity {
         search_btn.setOnClickListener(goSearchPage);
         go_mypage.setOnClickListener(goMypage);
         go_mystorage.setOnClickListener(goStorage);
-        databaseRefernece = FirebaseDatabase.getInstance().getReference();
-        models = new ArrayList<>();
     }
 
     private void initDatabase() {
@@ -98,17 +78,10 @@ public class MainActivity extends AppCompatActivity {
         databaseRefernece.child("contents").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                models.clear();
-
                 for (DataSnapshot homecontent : snapshot.getChildren()) {
-
-                    title = (String) homecontent.child("title").getValue();
-                    content = (String) homecontent.child("content").getValue();
-                    img =R.drawable.sample1;
-                    models.add(new Model(img, title,content));
-//                    showToast(content.child("title").getValue().toString());
+                    models.add(new Model(R.drawable.sample1, (String) homecontent.child("title").getValue(), (String) homecontent.child("content").getValue()));
                 }
-//                models.notify();
+                mainAdapter.notifyDataSetChanged();
 
             }
 
@@ -117,19 +90,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-//        models.add(new Model(R.drawable.sample1, "dd","dd"));
-//        models.add(new Model(R.drawable.sample1, "dd","dd"));
-
-//        mainAdapter = new MainAdapter(models, this);
-
     }
-
-    private void showToast(String str) {
-        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
-    }
-
-    private void startSignupActivity() {
+    private void startSignupActivity(){
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
