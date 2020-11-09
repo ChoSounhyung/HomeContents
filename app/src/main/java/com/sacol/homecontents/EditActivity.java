80450395bbeccd27c6c77e851c9d83f644787fe4
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-10 01:48:31 +0900

    Edit : onClick event, login activity text 추가

--- app/src/main/java/com/sacol/homecontents/EditActivity.java
+++ app/src/main/java/com/sacol/homecontents/EditActivity.java
@@ -3,12 +3,45 @@ package com.sacol.homecontents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
+    private TextView edit_back;
+    private TextView edit_completion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        init();
+        setUp();
     }
+
+    private void init() {
+        edit_back = findViewById(R.id.edit_back);
+        edit_completion = findViewById(R.id.edit_completion);
+    }
+
+    private void setUp() {
+        edit_back.setOnClickListener(goBackPage);
+        edit_completion.setOnClickListener(complete);
+    }
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+
+    View.OnClickListener complete = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+            Toast.makeText(EditActivity.this, "수정 되었습니다", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
