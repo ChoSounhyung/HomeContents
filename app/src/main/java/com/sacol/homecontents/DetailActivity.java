07e9bdc4a254413e674e0feba470a0b742ee1621
jimin-oh <wlals6828@daum.net>
2020-11-10 00:05:17 +0900

    DetailActivity save Icon 기능

--- app/src/main/java/com/sacol/homecontents/DetailActivity.java
+++ app/src/main/java/com/sacol/homecontents/DetailActivity.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import adapter.DetailAdapter;
@@ -39,6 +41,9 @@ public class DetailActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private TextView detail_nickname;
     private String uid;
+    private DatabaseReference mDatabase;
+    private String key;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +68,9 @@ public class DetailActivity extends AppCompatActivity {
         detail_contents =findViewById(R.id.detail_contents);
         detail_title = findViewById(R.id.detail_title);
         detail_nickname = findViewById(R.id.detail_nickname);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        uid = FirebaseAuth.getInstance().getUid().toString();
+
     }
 
     public void setUp() {
@@ -73,7 +81,6 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void initDatabase() {
-
         databaseReference.child("contents").child(date).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -83,9 +90,7 @@ public class DetailActivity extends AppCompatActivity {
                 detail_contents.setText(snapshot.child("content").getValue().toString());
                 detail_title.setText(snapshot.child("title").getValue().toString());
 
-                showToast(String.valueOf(models.size()));
                 detailAdapter.notifyDataSetChanged();
-
                 databaseReference.child("users").child(snapshot.child("uid").getValue().toString()).addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -97,6 +102,26 @@ public class DetailActivity extends AppCompatActivity {
 
                     }
                 });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        databaseReference.child("save").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot homecontent : snapshot.getChildren()) {
+                    if(date.equals(homecontent.getValue().toString())){
+                        save.setImageResource(R.drawable.storage_filled_icon);
+                        flag = true;
+                        key = homecontent.getKey();
+                    }
+                }
+
             }
 
             @Override
@@ -122,10 +147,13 @@ public class DetailActivity extends AppCompatActivity {
         public void onClick(View view) {
             if (!flag) {
                 save.setImageResource(R.drawable.storage_filled_icon);
+                mDatabase.child("save").child(uid).push().setValue(date);
                 flag = true;
 
             } else {
                 save.setImageResource(R.drawable.storage_icon);
+                mDatabase.child("save").child(uid).child(key).removeValue();
+
                 flag = false;
             }
         }
