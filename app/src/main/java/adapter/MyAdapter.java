579521d0ae21ba2f0a5f2cddcfff5a8cf4eb2a44
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-11 02:02:01 +0900

    Search Adapter 연결

--- app/src/main/java/adapter/MyAdapter.java
+++ app/src/main/java/adapter/MyAdapter.java
@@ -1,4 +1,60 @@
 package adapter;
 
-public class BaseAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.sacol.homecontents.Model;
+import com.sacol.homecontents.R;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends BaseAdapter {
+    Context context = null;
+    LayoutInflater layoutInflater = null;
+    ArrayList<Model> models = new ArrayList<Model>();
+
+    public MyAdapter(Context context, ArrayList<Model> data) {
+        this.context = context;
+        this.models = data;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    public void addItem(Model model) {
+        models.add(model);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return models.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup viewGroup) {
+        View v = layoutInflater.inflate(R.layout.search_item, null);
+
+        ImageView image = v.findViewById(R.id.search_image);
+        TextView title = v.findViewById(R.id.search_title);
+        TextView content = v.findViewById(R.id.search_content);
+
+        title.setText(models.get(position).getTitle());
+        content.setText(models.get(position).getCont());
+
+        return v;
+    }
 }
--- app/src/main/java/com/sacol/homecontents/SearchActivity.java
+++ app/src/main/java/com/sacol/homecontents/SearchActivity.java
@@ -2,12 +2,23 @@ package com.sacol.homecontents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import adapter.MyAdapter;
 
 public class SearchActivity extends AppCompatActivity {
     private TextView cancel_btn;
+    private ListView list;
+
+    ArrayList<Model> modelArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +31,27 @@ public class SearchActivity extends AppCompatActivity {
 
     public void init() {
         cancel_btn = findViewById(R.id.search_cancel);
+        list = findViewById(R.id.search_list);
+        final MyAdapter myAdapter = new MyAdapter(this, modelArrayList);
     }
 
     public void setUp() {
         cancel_btn.setOnClickListener(goBackPage);
+        list.setOnItemClickListener(clickItem);
     }
 
     View.OnClickListener goBackPage = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             SearchActivity.this.finish();
+        }
+    };
+
+    AdapterView.OnItemClickListener clickItem = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            Intent intent = new Intent(SearchActivity.this, DetailActivity.class);
+            startActivity(intent);
         }
     };
 }
