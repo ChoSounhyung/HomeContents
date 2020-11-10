443dad69eed27f971b614df16774bde471e83cb3
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-11 00:23:54 +0900

    detail edit 화면 구현

---
+++ app/src/main/java/com/sacol/homecontents/DetailEditActivity.java
@@ -0,0 +1,13 @@
+package com.sacol.homecontents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_edit);
+    }
+}
---
+++ app/src/main/java/com/sacol/homecontents/MypageEditActivity.java
@@ -0,0 +1,111 @@
+package com.sacol.homecontents;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 0;
+
+    private TextView edit_back;
+    private TextView edit_completion;
+    private ImageView edit_gallery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        init();
+        setUp();
+        checkSelfPermission();
+    }
+
+    private void init() {
+        edit_back = findViewById(R.id.edit_back);
+        edit_completion = findViewById(R.id.edit_completion);
+        edit_gallery = findViewById(R.id.edit_gallery);
+    }
+
+    private void setUp() {
+        edit_back.setOnClickListener(goBackPage);
+        edit_completion.setOnClickListener(complete);
+        edit_gallery.setOnClickListener(approach);
+    }
+
+    View.OnClickListener goBackPage = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+
+    View.OnClickListener complete = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+            Toast.makeText(EditActivity.this, "수정 되었습니다", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener approach = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent();
+            //기기 기본 갤러리 접근
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //권한을 허용 했을 경우
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    //동의
+                    Log.d("PlusActivity", "권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+    }
+
+    public void checkSelfPermission() {
+        String temp = "";
+
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+
+        if (TextUtils.isEmpty(temp) == false) {
+            //권한 요청
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+        } else {
+            //모두 허용 상태
+            Toast.makeText(this, "권한을 모두 허용하셨습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
