f4054067f0c2bad94d21d78504182a5eddc1162b
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-12 23:31:15 +0900

    splash 화면 구현

---
+++ app/src/main/java/com/sacol/homecontents/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.sacol.homecontents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000);     // 대기 초 설정
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
