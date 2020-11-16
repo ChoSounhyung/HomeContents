21f5f966b64782aaaae4e38e7e74079593cd31c7
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2020-11-16 13:23:19 +0900

    Personal Information 화면 구현

---
+++ app/src/main/java/kr/hs/emirim/homecontents/Personalinfo.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.homecontents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sacol.homecontents.R;
+
+public class Personalinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personalinfo);
+    }
+}
